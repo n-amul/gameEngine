2fada4be9ac837a1256fc7d77c67d19940231719
unknown <lee2322@wisc.edu>
2025-03-27 08:46:34 -0500

    input handler

--- engine/src/core/applcation.c
+++ engine/src/core/applcation.c
@@ -1,9 +1,13 @@
 #include "application.h"
+
+#include "platform/platform.h"
 #include "game_types.h"
 
 #include "logger.h"
 
-#include "platform/platform.h"
+#include "core/kmemory.h"
+#include "core/event.h"
+#include "core/input.h"
 
 typedef struct application_state {
     game* game_inst;
@@ -18,34 +22,50 @@ typedef struct application_state {
 static b8 initialized =FALSE;
 static application_state app_state;
 
+//event handlers
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context);
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context);
+
+
 b8 application_create(game* game_inst){
     if(initialized){
         KERROR("application is already created.");
         return FALSE;
     }
+
     app_state.game_inst=game_inst;
     //init subsystems
     initialize_logging();
+    input_initialize();
      // TODO: Remove this
-     KFATAL("A test message: %f", 3.14f);
-     KERROR("A test message: %f", 3.14f);
-     KWARN("A test message: %f", 3.14f);
-     KINFO("A test message: %f", 3.14f);
-     KDEBUG("A test message: %f", 3.14f);
-     KTRACE("A test message: %f", 3.14f);
+    KFATAL("A test message: %f", 3.14f);
+    KERROR("A test message: %f", 3.14f);
+    KWARN("A test message: %f", 3.14f);
+    KINFO("A test message: %f", 3.14f);
+    KDEBUG("A test message: %f", 3.14f);
+    KTRACE("A test message: %f", 3.14f);
+   
+    app_state.is_running=TRUE;
+    app_state.is_suspended=FALSE;
 
-     app_state.is_running=TRUE;
-     app_state.is_suspended=FALSE;
-
-     if(!platform_startup(
-        &app_state.platform,
-        game_inst->app_config.name,
-        game_inst->app_config.start_pos_x,
-        game_inst->app_config.start_pos_y,
-        game_inst->app_config.start_width,
-        game_inst->app_config.start_height)){
+    if(!event_initialize()) {
+        KERROR("Event system failed initialization. Application cannot continue.");
         return FALSE;
-     }
+    }
+    event_register(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_register(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_register(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
+
+
+    if(!platform_startup(
+       &app_state.platform,
+       game_inst->app_config.name,
+       game_inst->app_config.start_pos_x,
+       game_inst->app_config.start_pos_y,
+       game_inst->app_config.start_width,
+       game_inst->app_config.start_height)){
+       return FALSE;
+    }
 
      //init game
      if(!app_state.game_inst->initialize(app_state.game_inst)){
@@ -60,6 +80,7 @@ b8 application_create(game* game_inst){
 }
 
 b8 application_run(){
+    KINFO(get_memory_usage_str());
     while(app_state.is_running){
         if(!platform_pump_message(&app_state.platform)){
             app_state.is_running=FALSE;
@@ -72,15 +93,67 @@ b8 application_run(){
                 break;
             }
         }
-
+        //call the render routine
         if(!app_state.game_inst->render(app_state.game_inst,(f32)0)){
             KFATAL("Game render failed");
             app_state.is_running=FALSE;
             break;
         }
+         // NOTE: Input update/state copying should always be handled
+         // after any input should be recorded; I.E. before this line.
+         // As a safety, input is the last thing to be updated before
+         // this frame ends.
+        input_update(0);
     }
     app_state.is_running=FALSE;
+
+    // Shutdown event system.
+    event_unregister(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+    event_unregister(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+    event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
+    event_shutdown();
+    input_shutdown();
+    
     platform_shutdown(&app_state.platform);
 
     return TRUE;
+}
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context) {
+    switch (code) {
+        case EVENT_CODE_APPLICATION_QUIT: {
+            KINFO("EVENT_CODE_APPLICATION_QUIT recieved, shutting down.\n");
+            app_state.is_running = FALSE;
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context) {
+    if (code == EVENT_CODE_KEY_PRESSED) {
+        u16 key_code = context.data.u16[0];
+        if (key_code == KEY_ESCAPE) {
+            // NOTE: Technically firing an event to itself, but there may be other listeners.
+            event_context data = {};
+            event_fire(EVENT_CODE_APPLICATION_QUIT, 0, data);
+
+            // Block anything else from processing this.
+            return TRUE;
+        } else if (key_code == KEY_A) {
+            // Example on checking for a key
+            KDEBUG("Explicit - A key pressed!");
+        } else {
+            KDEBUG("'%c' key pressed in window.", key_code);
+        }
+    } else if (code == EVENT_CODE_KEY_RELEASED) {
+        u16 key_code = context.data.u16[0];
+        if (key_code == KEY_B) {
+            // Example on checking for a key
+            KDEBUG("Explicit - B key released!");
+        } else {
+            KDEBUG("'%c' key released in window.", key_code);
+        }
+    }
+    return FALSE;
 }
--- engine/src/entry.h
+++ engine/src/entry.h
@@ -2,7 +2,10 @@
  
  #include "core/application.h"
  #include "core/logger.h"
+ #include "core/kmemory.h"
+
  #include "game_types.h"
+ 
  
  // Externally-defined function to create a game.
  extern b8 create_game(game* out_game);
@@ -11,6 +14,7 @@
   * The main entry point of the application.
   */
  int main(void) {
+    initialize_memory();
      // Request the game instance from the application.
      game game_inst;
      if (!create_game(&game_inst)) {
@@ -35,6 +39,7 @@
          KINFO("Application did not shutdown gracefully.");
          return 2;
      }
+     shutdown_memory();
  
      return 0;
  }
